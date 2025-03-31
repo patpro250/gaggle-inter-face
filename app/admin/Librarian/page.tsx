cce1477ecc3a5116acc18c09c0d9a514ca6337c3
iraguha patrick <patrickiraguha95@gmail.com>
2025-03-31 09:10:17 +0200

    add not font and lay out to admin

---
+++ app/admin/Librarian/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Librarian = () => {
+  return (
+    <div>
+      <h1 className=" text-2xl ">Librarian page</h1>
+    </div>
+  );
+};
+
+export default Librarian;
---
+++ app/admin/layout.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+import NavBarAdminComponent from "../ui/adminNavBar/page";
+import AsiderPageAdmin from "../ui/adminAsider/page";
+
+interface Props {
+  children: ReactNode;
+}
+const AdiminLayout = ({ children }: Props) => {
+  return (
+    <div className=" h-[100vh] overflow-hidden ">
+      <NavBarAdminComponent />
+
+      <main className="overflow-y-hidden h-[100vh]  flex   ">
+        <div>
+          <AsiderPageAdmin />
+        </div>
+        <div className=" w-[90%] bg-gray-50 p-4  overflow-y-auto h-[100vh] ">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default AdiminLayout;
