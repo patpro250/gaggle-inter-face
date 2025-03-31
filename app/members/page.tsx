15ce52c2beb62f7ad9353b0b6b4e60ca72ba1510
Terry <madeinterry@gmx.us>
2025-03-31 05:18:48 +0200

    Fix frammer-motion

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,13 +1,12 @@
 import { Container } from "@radix-ui/themes";
 import Search from "./Search";
 import Recommended from "./Recommended";
-import { motion } from "framer-motion";
 
 const BooksPage = () => {
   return (
     <Container>
       <h1 className="members-title">Books</h1>
-      <p className="members-paragraph">All your favorites is here</p>
+      <p className="members-paragraph">All your favorites are here</p>
       <Search />
       <Recommended />
     </Container>
