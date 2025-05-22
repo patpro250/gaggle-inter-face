2c7d9929f27aa48add0bc44ee8af151ca5f56ec6
Terry <madeinterry@gmx.us>
2025-05-22 16:27:28 +0200

    Something added on Home page.

--- app/component/web/burner/page.tsx
+++ app/component/web/burner/page.tsx
@@ -16,6 +16,15 @@ const HeroBanner = () => {
             improve reading skills, and bring technology to every corner of the
             learning experience.
           </p>
+<div className="max-w-2xl mx-auto px-4 py-6">
+  <p className="font-sans text-lg text-gray-800 dark:text-primary leading-relaxed tracking-wide">
+    <strong className="text-blue-600 dark:text-blue-400">NITIBOOK</strong> For Pricing Page
+NITIBOOK is built with care, dedication, and significant investment. To keep providing a high-quality experience, we rely on fair pricing that reflects the real value and resources behind the service. For About or Mission Page
+NITIBOOK represents years of effort, thoughtful design, and over 1 million Rwf invested into building a meaningful platform. We're proud to offer a premium experience — not as an entitlement, but as a result of purposeful work. Sign-up
+NITIBOOK is more than just a platform — it’s a service crafted with intention and supported by real people and real resources. We welcome you into a space that values quality, sustainability, and respect.
+  </p>
+</div>
+
           <Link
             href="../../../d/auth12/s"
             className="inline-block border-2 text-sm border-gray-200 bg-indigo-400 hover:bg-indigo-300 text-gray-700 font-normal py-2 px-6 rounded-md  transition"
