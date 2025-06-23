e86b0f1dc1390524d2c7284c73d1471b133c0b25
Terry <madeinterry@gmx.us>
2025-06-23 23:11:41 +0200

    Fix formatting issues in layout component for improved readability

--- app/signup/layout.tsx
+++ app/signup/layout.tsx
@@ -1,16 +1,15 @@
 import React, { ReactNode } from "react";
 import Link from "next/link";
-import { ArrowLeft } from "lucide-react";
 interface Props {
   children: ReactNode;
 }
 const layout = ({ children }: Props) => {
   return (
     <div>
-  "<Link href={"/"}>
+  <Link href={"/"}>
         <button className=" bg-primary font-semibold text-white rounded-md py-1 px-2 m-2  ">
           Back to Home
-        </button>"
+        </button>
 </Link>
 
       {/* <Navbar /> */}
