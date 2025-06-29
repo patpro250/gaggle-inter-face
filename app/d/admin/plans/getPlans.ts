a9ba67e6ac41e0b679815d832bbb3e29bf030a3d
Yves-Developer <yvesmugisha09@gmail.com>
2025-06-29 23:34:24 +0200

    Optimized Builds

--- app/d/admin/plans/getPlans.ts
+++ app/d/admin/plans/getPlans.ts
@@ -1,10 +1,18 @@
 "use server";
-// import { getApiClient } from "@/app/g/schools/axios";
 import axios from "axios";
+import http from "http";
+import https from "https";
+
+const httpAgent = new http.Agent({ family: 4 });
+const httpsAgent = new https.Agent({ family: 4 });
 
 export const fetchPlans = async () => {
   const api = process.env.NEXT_PUBLIC_API_URL;
 
-  const response = await axios.get(`${api}/plans`);
+  const response = await axios.get(`${api}/plans`, {
+    httpAgent,
+    httpsAgent,
+  });
+
   return response.data;
 };
